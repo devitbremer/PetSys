b1e4dc3422b3ad876074adc118a94401683f9cc8
Devit Bremer <devitbremer@gmail.com>
2016-04-03 16:52:51 -0300

    updates2
    updates2

--- menu.php
+++ menu.php
@@ -1,17 +1,23 @@
-<ul id="nav-mobile" class="side-nav fixed">
-        <li class="bold"><a href="about.html" class="waves-effect waves-teal">Sobre</a></li>
+<header>
+      <ul id="nav-mobile" class="side-nav fixed grey darken-2" style="transform: translateX(0%);">
+        <!--<li class="logo"><a id="logo-container" href="http://materializecss.com/" class="brand-logo">
+            <object id="front-page-logo" type="image/svg+xml" data="res/materialize.svg">Your browser does not support SVG</object></a></li>-->
+        <li class="bold grey darken-2"><a href="#" class="waves-effect waves-teal white-text hoverable">Sobre</a></li>
         <li class="no-padding">
           <ul class="collapsible collapsible-accordion">
-            <li class="bold"><a class="collapsible-header  waves-effect waves-teal">Cadastros</a>
+            <li class="bold grey darken-2"><a class="collapsible-header  waves-effect waves-teal white-text hoverable">Cadastros</a>
               <div class="collapsible-body" style="">
                 <ul>
-                  <li><a href="produtos.php">Produtos</a></li>
-                  <li><a href="clientes.php">Clientes</a></li>
-                  <li><a href="pets.php">Pets</a></li>
-                  <li><a href="servicos.php">Serviços</a></li>
+                  <li class="bold grey darken-2"><a class="waves-effect waves-teal white-text hoverable" href="#">Clientes</a></li>
+                  <li class="bold grey darken-2"><a class="waves-effect waves-teal white-text hoverable" href="#">Produtos</a></li>
+                  <li class="bold grey darken-2"><a class="waves-effect waves-teal white-text hoverable" href="#">Serviços</a></li>
+                  <li class="bold grey darken-2"><a class="waves-effect waves-teal white-text hoverable" href="#">Pets</a></li>
                 </ul>
               </div>
             </li>
-        <li class="bold"><a href="venda.php" class="waves-effect waves-teal">Vendas</a></li>
-        <li class="bold"><a href="contas_a_pagar.php" class="waves-effect waves-teal">Contas a Pagar</a></li>
-    </ul>
+          </ul>
+        </li>
+        <li class="bold grey darken-2"><a href="#" class="waves-effect waves-teal white-text hoverable">Vendas</a></li>
+        <li class="bold grey darken-2"><a href="#" class="waves-effect waves-teal white-text hoverable">Contas a Pagar</a></li>
+      </ul>
+    </header>
--- scripts.php
+++ scripts.php
@@ -1,2 +1,12 @@
 <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
 <script type="text/javascript" src="materialize/js/materialize.min.js"></script>
+<script type="text/javascript">
+	$(document).ready(function() {
+    $('select').material_select();
+  });
+
+	$('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15 // Creates a dropdown of 15 years to control year
+  });
+</script>
