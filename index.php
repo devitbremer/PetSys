f6a1db9c271208b7d4b1426d0696ddab3a2e58d2
Devit Bremer <devitbremer@gmail.com>
2016-04-03 10:54:53 -0300

    core-based

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+  <html>
+    <?php
+        include("head.php");
+      ?>
+
+    <body>
+      <?php
+        include("menu.php");
+      ?> 
+      <!--Conteudo vai aqui em baixo-->
+
+      <!--Conteudo teermina aqui -->
+      <?php
+        include("scripts.php");
+      ?> 
+    </body>
+  </html>
---
+++ menu.php
@@ -0,0 +1,17 @@
+<ul id="nav-mobile" class="side-nav fixed">
+        <li class="bold"><a href="about.html" class="waves-effect waves-teal">Sobre</a></li>
+        <li class="no-padding">
+          <ul class="collapsible collapsible-accordion">
+            <li class="bold"><a class="collapsible-header  waves-effect waves-teal">Cadastros</a>
+              <div class="collapsible-body" style="">
+                <ul>
+                  <li><a href="produtos.php">Produtos</a></li>
+                  <li><a href="clientes.php">Clientes</a></li>
+                  <li><a href="pets.php">Pets</a></li>
+                  <li><a href="servicos.php">Serviços</a></li>
+                </ul>
+              </div>
+            </li>
+        <li class="bold"><a href="venda.php" class="waves-effect waves-teal">Vendas</a></li>
+        <li class="bold"><a href="contas_a_pagar.php" class="waves-effect waves-teal">Contas a Pagar</a></li>
+    </ul>
