94dfd0f5b98e4edfec30ea58fe009ce5b4cdccc6
Devit Bremer <devitbremer@gmail.com>
2016-04-03 16:56:11 -0300

    updates3

--- index.php
+++ index.php
@@ -13,78 +13,7 @@
           <div class="container">
             <div>
               <!--Conteudo vai aqui em baixo-->
-                <div class="row">
-                  <form class="col s12">
-                    <div class="row">
-                      <div class="input-field col s5">
-                        <input id="nome" type="text" class="validate">
-                        <label for="nome">Nome</label>
-                      </div>
-                      <div class="input-field col s3">
-                        <input id="cpf" type="text" class="validate">
-                        <label for="cpf">CPF</label>
-                      </div>
-                      <div class="input-field col s2">
-                        <input id="data_nasc" type="text" class="validate">
-                        <label for="data_nasc">Nasc.</label>
-                      </div>
-                      <div class="input-field col s2">
-                        <select>
-                          <option value="" disabled selected>Selecione o sexo</option>
-                          <option value="1">Masculino</option>
-                          <option value="2">Feminino</option>
-                        </select>
-                        <label>Sexo</label>
-                    </div>
-                    <div class="row">
-                      <div class="input-field col s5">
-                        <input id="rua" type="text" class="validate">
-                        <label for="rua">Rua</label>
-                      </div>
-                      <div class="input-field col s3">
-                        <input id="numero" type="text" class="validate">
-                        <label for="numero">Numero</label>
-                      </div>
-                      <div class="input-field col s4">
-                        <input id="bairro" type="text" class="validate">
-                        <label for="bairro">Bairro</label>
-                      </div>
-                    </div>
-                    <div class="row">
-                      <div class="input-field col s3">
-                        <input id="cep" type="text" class="validate">
-                        <label for="cep">CEP</label>
-                      </div>
-                      <div class="input-field col s5">
-                        <input id="cidade" type="text" class="validate">
-                        <label for="cidade">Cidade</label>
-                      </div>
-                      <div class="input-field col s4">
-                        <select>
-                          <option value="" disabled selected>Escolha um Estado</option>
-                          <option value="1">Paraná</option>
-                          <option value="2">Santa Catarina</option>
-                          <option value="3">Rio Grande do SUl</option>
-                        </select>
-                        <label>Estado</label>
-                      </div>
-                    </div>
-                    <div class="row">
-                      <div class="input-field col s3">
-                        <input id="telefone" type="text" class="validate">
-                        <label for="telefone">Telefone</label>
-                      </div>
-                      <div class="input-field col s3">
-                        <input id="celular" type="text" class="validate">
-                        <label for="celular">Celular</label>
-                      </div>
-                       <div class="input-field col s6">
-                        <input id="email" type="email" class="validate">
-                        <label for="email">Email</label>
-                      </div>
-                    </div>
-                  </form>
-                </div>
+                <?php include("clientes.php");?>
               <!--Conteudo termina aqui -->
             </div>
           </div>
