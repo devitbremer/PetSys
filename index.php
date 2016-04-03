763b46e53f7d2efb26506f141f0734d8eef8a3fb
Devit Bremer <devitbremer@gmail.com>
2016-04-03 17:50:57 -0300

    Revert "Delete index.php"
    This reverts commit 34141f45c2461567e6b7f853a9e14d81541e1b6d.

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+  <html>
+    <?php
+        include("head.php");
+      ?>
+
+    <body>
+      <?php
+        include("menu.php");
+      ?> 
+      
+        <main>
+          <div class="container">
+            <div>
+              <!--Conteudo vai aqui em baixo-->
+                <?php include("clientes.php");?>
+              <!--Conteudo termina aqui -->
+            </div>
+          </div>
+        </main>
+      <?php
+        include("scripts.php");
+      ?> 
+    </body>
+  </html>
